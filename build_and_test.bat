d0d3930c6769f1d2bfc0a8a671a51783cf90a2c8
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2018-02-28 20:54:05 +0000

    Specify consistent architecture for cmake and conan

--- build_and_test.bat
+++ build_and_test.bat
@@ -4,8 +4,8 @@ cd    build-windows
 REM Commands based on:
 REM     https://stackoverflow.com/questions/45835644/how-to-link-only-to-some-of-the-libraries-in-conan-package
 REM Maybe add -s compiler.version=15 ?
-conan install .. -g cmake_multi -s build_type=Debug   -s compiler.runtime=MDd
-conan install .. -g cmake_multi -s build_type=Release -s compiler.runtime=MD
+conan install .. -g cmake_multi -s arch=x86_64 -s build_type=Debug   -s compiler.runtime=MDd
+conan install .. -g cmake_multi -s arch=x86_64 -s build_type=Release -s compiler.runtime=MD
 cmake -G "Visual Studio 15 2017 Win64" ..
 
 cmake --build . --config Release
